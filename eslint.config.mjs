37465e0a2a23d36051560bc404eab76e202d044b
ubugeeei <ubuge1122@gmail.com>
2025-03-03 00:21:18 +0900

    cfg: eslint-plugin-vuejs-accessibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
+import pluginVueA11y from "eslint-plugin-vuejs-accessibility";
 
-export default withNuxt().override("nuxt/vue/single-root", {
-  rules: { "vue/no-multiple-template-root": "off" },
-});
+export default withNuxt()
+  .append(...pluginVueA11y.configs["flat/recommended"])
+  .override("nuxt/vue/single-root", {
+    rules: { "vue/no-multiple-template-root": "off" },
+  });
 // Your custom configs here
