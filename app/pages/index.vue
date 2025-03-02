ff587f0c8790b7b6bea18999d63932d67168d4dd
ubugeeei <ubuge1122@gmail.com>
2025-03-03 00:03:06 +0900

    cfg: lightningcss

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,14 @@
 <template>
   <h1>Hello vuefes-2025</h1>
+  <div class="container">
+    <p>I'm ubugeeei</p>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  p {
+    color: red;
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   future: { compatibilityVersion: 4 },
   imports: { autoImport: false },
+  vite: {
+    css: {
+      transformer: "lightningcss",
+      lightningcss: {
+        nonStandard: {
+          deepSelectorCombinator: true,
+        },
+      },
+    },
+  },
 });
