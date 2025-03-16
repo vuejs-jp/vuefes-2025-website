a54a6847ef84d0044cac08562a1e476d13a46e33
ubugeeei <71201308+ubugeeei@users.noreply.github.com>
2025-03-17 01:03:35 +0900

    feat: parts component (#152)

---
+++ app/components/heading/Heading.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineSlots<{
+  default: () => unknown;
+}>();
+</script>
+
+<template>
+  <h1 v-bind="$attrs"><slot /></h1>
+  <hr />
+</template>
+
+<style scoped>
+h1::before {
+  content: "・";
+}
+
+hr {
+  border-top: 1px solid var(--color-divider);
+  border-bottom: none;
+  border-left: none;
+  border-right: none;
+  margin: 0.5em;
+}
+</style>
