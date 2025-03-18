ce002e2805d7b5f87d104ddf685270f0e0197ee0
ubugeeei <71201308+ubugeeei@users.noreply.github.com>
2025-03-18 23:46:12 +0900

    feat: 404 (#178)

---
+++ app/error.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { navigateTo, type NuxtError } from "#app";
+
+const props = defineProps<{ error: NuxtError }>();
+
+if (props.error.statusCode === 404) {
+  navigateTo("/");
+}
+</script>
