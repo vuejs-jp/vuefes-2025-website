afd8a6d331b810e3cd28ca06aa87d709108e070d
ubugeeei <ubuge1122@gmail.com>
2025-03-22 06:07:47 +0900

    chore: remove unused var

--- app/components/mainVisual/MainVisual.vue
+++ app/components/mainVisual/MainVisual.vue
@@ -15,7 +15,7 @@ onMounted(() => {
       window.WebGLRenderingContext &&
       (canvas.getContext("webgl") || canvas.getContext("experimental-webgl"))
     );
-  } catch (_e) {
+  } catch {
     isWebGLSupported.value = false;
   }
 });
