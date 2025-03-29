900fe582ffa9d0eb8f3d04a5eabe31a83fdcd22c
ubugeeei <ubuge1122@gmail.com>
2025-03-29 22:14:22 +0900

    chore: ssr deploy test

---
+++ app/pages/ssr-test.vue
@@ -0,0 +1,10 @@
+<!-- NOTE: Netlify で SSR が動いてるか確認する CSRP -->
+<script>
+let count = 1;
+</script>
+
+<script setup>
+const countRef = ref(++count);
+</script>
+
+<template>ssr test! {{ countRef }}</template>
