4c69dc02d0a2a4c4f967712d2170aea1851eb5b3
kanno_stanfoot(genj1) <39039737+genj11@users.noreply.github.com>
2025-03-30 00:31:34 +0900

    chore: configure runtime baseURL (#264)
    * chore: configure runtime baseURL
    
    * chore: add build command
    
    * fix: remove redirects setting
    
    * update: netlify.toml for redirects
    
    * add: comments
    
    ---------
    
    Co-authored-by: ktym4a <shoma@ktym4a.me>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { FileSystemIconLoader } from "unplugin-icons/loaders";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: process.env.NODE_ENV === "production" ? "/2025/" : "/",
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
