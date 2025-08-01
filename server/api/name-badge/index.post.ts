92b9ae439da20ab7aed7811ef1f83e085c06268b
ubugeeei <ubuge1122@gmail.com>
2025-08-01 12:50:26 +0900

    fix: r2 encode key (#796)

--- server/api/name-badge/index.post.ts
+++ server/api/name-badge/index.post.ts
@@ -91,7 +91,7 @@ export default defineEventHandler(async (event) => {
 
     const command = new PutObjectCommand({
       Bucket: process.env.CLOUDFLARE_R2_BUCKET_NAME!,
-      Key: objectName,
+      Key: encodeURI(objectName),
       ContentType: validatedBody.data.avatarImageBlob.type,
       Body: Buffer.from(await validatedBody.data.avatarImageBlob.arrayBuffer()),
     });
