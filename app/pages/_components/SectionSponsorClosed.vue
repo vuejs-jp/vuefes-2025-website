e81bbd392fed71627e08ad3899f36740b800a0be
naokihaba <naosprintrunner6385@gmail.com>
2025-10-02 03:51:17 +0900

    feat: add SectionSponsorClosed component for sponsor closed messages

---
+++ app/pages/_components/SectionSponsorClosed.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { HOME_HEADING_ID } from "~/constant";
+import { useBreakpoint, useI18n, useWithBase } from "#imports";
+import { VFSection, JaSponsorClosed, EnSponsorClosed } from "#components";
+
+const bp = useBreakpoint();
+const withBase = useWithBase();
+const { locale, t } = useI18n();
+</script>
+
+<template>
+  <VFSection
+    :id="HOME_HEADING_ID.sponsorClosed"
+    :title="t('sponsorClosed')"
+    :cover-image="{
+      src: bp === 'pc'
+        ? withBase('/images/top/cover/sponsor-wanted-pc.svg') // TODO: 画像差し替え
+        : withBase('/images/top/cover/sponsor-wanted-sp.svg'),
+      alt: t('sponsorWantedCoverImageAlt'),
+    }"
+  >
+    <component :is="locale === 'ja' ? JaSponsorClosed : EnSponsorClosed" />
+  </VFSection>
+</template>
