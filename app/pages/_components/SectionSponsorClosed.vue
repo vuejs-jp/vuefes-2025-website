beec2e60bcae6577d80fc1d70dfe084453f0f053
NaokiHaba <naosprintrunner6385@gmail.com>
2025-10-02 18:57:20 +0900

    refactor: simplify SectionSponsorClosed component by removing unused imports and props

--- app/pages/_components/SectionSponsorClosed.vue
+++ app/pages/_components/SectionSponsorClosed.vue
@@ -1,24 +1,13 @@
 <script setup lang="ts">
 import { HOME_HEADING_ID } from "~/constant";
-import { useBreakpoint, useI18n, useWithBase } from "#imports";
+import { useI18n } from "#imports";
 import { VFSection, JaSponsorClosed, EnSponsorClosed } from "#components";
 
-const bp = useBreakpoint();
-const withBase = useWithBase();
 const { locale, t } = useI18n();
 </script>
 
 <template>
-  <VFSection
-    :id="HOME_HEADING_ID.sponsorClosed"
-    :title="t('sponsorClosed')"
-    :cover-image="{
-      src: bp === 'pc'
-        ? withBase('/images/top/cover/sponsor-wanted-pc.svg') // TODO: 画像差し替え
-        : withBase('/images/top/cover/sponsor-wanted-sp.svg'),
-      alt: t('sponsorWantedCoverImageAlt'),
-    }"
-  >
+  <VFSection :id="HOME_HEADING_ID.sponsorClosed" :title="t('sponsorClosed')">
     <component :is="locale === 'ja' ? JaSponsorClosed : EnSponsorClosed" />
   </VFSection>
 </template>
