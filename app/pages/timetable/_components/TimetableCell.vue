c0169a5c7da397ebfb02d9aa54ecff8ba6e73683
OKI Yoshiya <oki.yoshiya@ceroan.co.jp>
2025-10-02 16:56:52 +0900

    feat: update timetable (#1027)
    * Windows環境でも動作するように修正した
    
    * タイムテーブルを最適化した
    
    * fix lint

--- app/pages/timetable/_components/TimetableCell.vue
+++ app/pages/timetable/_components/TimetableCell.vue
@@ -96,9 +96,17 @@ const hoverColor = `var(--color-${accentColorName.value}-accent-hover)`;
                 <div v-if="speaker.avatarUrl" class="avatar">
                   <img :src="speaker.avatarUrl" :alt="speaker.name">
                 </div>
-                <p class="name">
-                  {{ speaker.name }}
-                </p>
+                <div class="speaker-info">
+                  <p v-if="speaker.affiliation" class="affiliation">
+                    {{ speaker.affiliation }}
+                  </p>
+                  <p v-if="speaker.title" class="job-title">
+                    {{ speaker.title }}
+                  </p>
+                  <p class="name">
+                    {{ speaker.name }}
+                  </p>
+                </div>
               </div>
             </div>
           </template>
@@ -193,19 +201,37 @@ const hoverColor = `var(--color-${accentColorName.value}-accent-hover)`;
 
 .speaker-item {
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   gap: 0 8px;
   margin-top: 8px;
   .avatar{
-    width: 32px;
+    width: 48px;
     aspect-ratio: 1;
     overflow: hidden;
     border-radius: 8px;
     background-color: #fff;
   }
+  .speaker-info {
+    display: flex;
+    flex-direction: column;
+    gap: 0;
+  }
+  .affiliation {
+    font-size: 11px;
+    color: var(--color-text-default);
+    line-height: 1;
+    margin-bottom: 0.25rem;
+  }
+  .job-title {
+    font-size: 11px;
+    color: var(--color-text-default);
+    line-height: 1;
+    margin-bottom: 0.25rem;
+  }
   .name{
     font-size: 14px;
     color: var(--color-text-default);
+    line-height: 1.2;
   }
 }
 
@@ -217,7 +243,7 @@ const hoverColor = `var(--color-${accentColorName.value}-accent-hover)`;
 }
 
 .event-speaker {
-  &:has(a) {
+  &:has(a):not(:first-child) {
     margin-top: 24px;
   }
 }
