d304dbe45c43018d72d99368e97e894c424b26db
Naoki Haba <59875779+naokihaba@users.noreply.github.com>
2025-12-08 11:29:49 +0900

    feat: add photo section (#1095)
    * feat: add photo section with categories and description for Vue Fes Japan 2025
    
    * feat: add photo section with categories and description for Vue Fes Japan 2025
    
    * feat: add photo section identifier to HOME_HEADING_ID
    
    * feat: add photo categories and URLs for Vue Fes Japan 2025
    
    * feat: add photo section with categories and descriptions for Vue Fes Japan 2025
    
    * feat: create SectionPhoto component with categories and links for Vue Fes Japan 2025
    
    * feat: add SectionPhoto component to index page and update menu items
    
    * feat: import and add SectionPhoto component to the index page
    
    * feat: import useHead to prevent undefined error in photo section
    
    * feat: update photo section descriptions for clarity and usage guidance
    
    * feat: add preparation category and update photo section for Vue Fes Japan 2025

---
+++ i18n/photo.ts
@@ -0,0 +1,75 @@
+export interface PhotoCategory {
+  id: string;
+  url: string;
+}
+
+export const PHOTO_CATEGORIES: PhotoCategory[] = [
+  {
+    id: "preparation",
+    url: "https://photos.app.goo.gl/cdGW4p23ee9rTXZg8",
+  },
+  {
+    id: "beforeOpening",
+    url: "https://photos.app.goo.gl/ugQWo5ETx5gu6bKh7",
+  },
+  {
+    id: "reception",
+    url: "https://photos.app.goo.gl/HTmNHKnz6UjmG95m8",
+  },
+  {
+    id: "creativeWall",
+    url: "https://photos.app.goo.gl/Qt7TnDLjHpSjNcrPA",
+  },
+  {
+    id: "installation",
+    url: "https://photos.app.goo.gl/yFbpQKxCSv8RYL8o9",
+  },
+  {
+    id: "hacomonoTrack",
+    url: "https://photos.app.goo.gl/ZWhYEphawFvVksve8",
+  },
+  {
+    id: "matesTrack",
+    url: "https://photos.app.goo.gl/LNhyX8vpPXTjn3797",
+  },
+  {
+    id: "futureArchitectTrack",
+    url: "https://photos.app.goo.gl/hHiWQ9YKbn57NYpEA",
+  },
+  {
+    id: "cyberAgentTrack",
+    url: "https://photos.app.goo.gl/7EZgoqSaraMwFr4m9",
+  },
+  {
+    id: "sponsorBooth",
+    url: "https://photos.app.goo.gl/KWXZic2BP8F3aGe5A",
+  },
+  {
+    id: "vueFesStore",
+    url: "https://photos.app.goo.gl/Lw6uRduo3ReDHAj66",
+  },
+  {
+    id: "festivalCorner",
+    url: "https://photos.app.goo.gl/yBA81jYVDEj7MJ2w5",
+  },
+  {
+    id: "tattooSpace",
+    url: "https://photos.app.goo.gl/i7pdHCzkES6eeJV26",
+  },
+  {
+    id: "freeDrinkAndSnack",
+    url: "https://photos.app.goo.gl/Kt37arT22uuQzpaU7",
+  },
+  {
+    id: "restAndChargingSpace",
+    url: "https://photos.app.goo.gl/vcyCVG3jjdUhtBTaA",
+  },
+  {
+    id: "afterParty",
+    url: "https://photos.app.goo.gl/uYCj26EChbK3rbwSA",
+  },
+  {
+    id: "otherSnapshots",
+    url: "https://photos.app.goo.gl/FzAxRc6VmmHn9DAn8",
+  },
+];
